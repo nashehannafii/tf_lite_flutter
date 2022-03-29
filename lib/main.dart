5f476edae0e16816078b13ecd0a8eabd1022f84c
nashehannafii <nashehannafii@gmail.com>
2022-03-29 15:56:16 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home.dart';
+import 'package:camera/camera.dart';
+
+List<CameraDescription>? cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
